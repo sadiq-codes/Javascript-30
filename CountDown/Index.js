15e01dffd296b0d45d01de08824558af5b813f57
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-08-10 23:31:23 +0100

    create time left

---
+++ CountDown/Index.js
@@ -0,0 +1,38 @@
+let countdown;
+
+
+
+
+
+
+
+
+function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+  
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+  
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      // check if we should stop it!
+      if(secondsLeft < 0) {
+        clearInterval(countdown);
+        return;
+      }
+      // display it
+      displayTimeLeft(secondsLeft);
+    }, 1000);
+  }
+
+  
+  function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+  }
