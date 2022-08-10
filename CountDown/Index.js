8cd9cafa03dddefd0ee61ea69f94ca6c9171427a
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-08-10 23:32:45 +0100

    create start timer

--- CountDown/Index.js
+++ CountDown/Index.js
@@ -1,10 +1,7 @@
 let countdown;
-
-
-
-
-
-
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 
 function timer(seconds) {
@@ -45,4 +42,18 @@ function displayEndTime(timestamp) {
     const minutes = end.getMinutes();
     endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
   }
+
+  function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+  }
+  
+  buttons.forEach(button => button.addEventListener('click', startTimer));
+  document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+  });
   
