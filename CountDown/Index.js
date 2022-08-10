f3715606ad8e072d8172da8a5d190d2d0475d945
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-08-10 23:32:02 +0100

    create time end

--- CountDown/Index.js
+++ CountDown/Index.js
@@ -36,3 +36,13 @@ function timer(seconds) {
     document.title = display;
     timerDisplay.textContent = display;
   }
+
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  }
+  
